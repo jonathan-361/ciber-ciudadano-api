fe08d736078e3fcb7cd739b5494139926ab6da04
jonathan-361 <jonathanlopezuc@gmail.com>
2026-02-11 23:44:29 -0600

    Add README and sql script

---
+++ README.md
@@ -0,0 +1,46 @@
+# Ciber ciudadano API
+
+**Nota:** El archivo llamado _ciber_ciudadano.sql_ contiene el script para crear la base de datos y la tabla necesaria para trabajar.
+
+- [Repositorio de FrontEnd](https://github.com/jonathan-361/ciber-ciudadano.git)
+
+---
+
+### Endpoints
+
+- #### Iniciar sesión:
+
+**POST:** `http://localhost:3000/api/login`
+
+**JSON:**
+
+```
+{
+  "email": "johndoe777@gmail.com",
+  "password": "Johnny361"
+}
+```
+
+<br>
+
+- #### Registrar usuario:
+
+**POST:** `http://localhost:3000/api/users/register`
+
+**JSON:**
+
+```
+{
+  "name": "John",
+  "last_name": "Doe",
+  "email": "johndoe777@gmail.com",
+  "password": "Johnny361",
+  "birth_date": "2003-08-04"
+}
+```
+
+<br>
+
+- #### Obtener usuarios:
+
+**GET:** `http://localhost:3000/api/users`
---
+++ ciber_ciudadano.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-02-2026 a las 06:23:37
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ciber_ciudadano`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birth_date` date NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `last_name`, `email`, `password`, `birth_date`, `created_at`, `updated_at`) VALUES
+(4, 'Jonathan Alexander', 'Concha Chan', 'jonathan365@gmail.com', '$2b$10$CPaY/f4fxmTTdAtshdTISeyuhSa0h0PTkhXOv6/knKik.JpIaWRtK', '2004-10-26', '2026-02-12 04:47:02', '2026-02-12 05:01:40');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
